037a6892705a370e4ac069d1719d306e82f08b76
heiskaapo <aapo.heiska@gmail.com>
2023-03-11 15:39:01 +0200

    minor change

--- 1webserver.js
+++ 1webserver.js
@@ -1,6 +1,6 @@
 const http = require('node:http');
 const server = http.createServer((req, res)=>{
-const text_res = (req.url == '/') ? "You are now at the home page" : "you have reached some other place than the page you though first!"
+const text_res = (req.url == '/') ? "You are now at the home page" : "you have reached some other place the page you though first!"
   res.end(text_res)
 })
-server.listen(3000) 
+server.listen(3000)
